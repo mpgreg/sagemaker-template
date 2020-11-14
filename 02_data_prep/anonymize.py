e1605b50773de5cfaa599060e843ad81c2f4a0dd
EC2 Default User <ec2-user@ip-172-16-22-50.eu-west-1.compute.internal>
2020-11-14 11:53:12 +0000

    added pyspark and glue option

---
+++ 02_data_prep/anonymize.py
@@ -0,0 +1,38 @@
+from pyspark.context import SparkContext
+from pyspark.sql.functions import sha2, concat_ws
+from awsglue.context import GlueContext
+import sys
+
+def main():
+    spark = SparkSession.builder.appName("PySparkApp").getOrCreate()
+    args_iter = iter(sys.argv[1:])
+    args = dict(zip(args_iter, args_iter))
+    #args = {'project_bucket': 'project1-lz', 'input_table': 'upload', 'output_table': 'raw', 'database': 'default'}
+
+    project_bucket = args['project_bucket']
+    input_table = args['input_table']
+    output_table = args['output_table']
+    database = args['database']
+    output_s3_uri = 's3://' + project_bucket + "/" + output_table + '/'
+
+    # Create a Glue context
+    glueContext = GlueContext(SparkContext.getOrCreate())
+
+    # Create a DynamicFrame using the 'persons_json' table
+    df = glueContext.create_data_frame.from_catalog(database=database, table_name=input_table)
+
+    # Print out information about this data
+    #print("Count:  ", df.count())
+    #df.printSchema()
+
+    # replace each tweeters name with crc bigint
+    dfAnnocrc = df.withColumn("annonym", sha2("name", 256)).select("annonym", "tweet_id", "airline", "airline_sentiment", "text")
+
+    # write back to s3 as parquet
+    dfAnnocrc.write.mode("append").parquet(output_s3_uri)
+
+
+
+if __name__ == "__main__":
+    main()
+
