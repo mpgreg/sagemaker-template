809d48ad33534ab6e5879c4ffcccea25c6cf1fa2
EC2 Default User <ec2-user@ip-172-16-22-50.eu-west-1.compute.internal>
2020-11-14 14:09:26 +0000

    pyspark working

--- 02_data_prep/anonymize.py
+++ 02_data_prep/anonymize.py
@@ -1,29 +1,35 @@
-from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
 from pyspark.sql.functions import sha2, concat_ws
-from awsglue.context import GlueContext
 import sys
 
 def main():
-    spark = SparkSession.builder.appName("PySparkApp").getOrCreate()
+    spark = SparkSession.builder.appName("Anonymize PySpark").getOrCreate()
     args_iter = iter(sys.argv[1:])
     args = dict(zip(args_iter, args_iter))
-    #args = {'project_bucket': 'project1-lz', 'input_table': 'upload', 'output_table': 'raw', 'database': 'default'}
+    
+    #sample args for interactive testing
+    #args = {'project_bucket': 'project1-lz', 'input_table': 'upload', 'output_table': 'raw', 'database': 'default', 'file_name': 'Tweets.csv'}
 
     project_bucket = args['project_bucket']
     input_table = args['input_table']
     output_table = args['output_table']
     database = args['database']
-    output_s3_uri = 's3://' + project_bucket + "/" + output_table + '/'
+    input_s3_uri = 's3://' + project_bucket + '/' + input_table + '/' + args['file_name']
+    output_s3_uri = 's3://' + project_bucket + '/' + output_table + '/' + args['file_name'].split('.')[0] + '-anon/'
 
-    # Create a Glue context
-    glueContext = GlueContext(SparkContext.getOrCreate())
+    # Interactive pyspark from glue development endpoint allows reaading from glue crawlers
+    # from awsglue.context import GlueContext
+    #glueContext = GlueContext(SparkContext.getOrCreate())
 
-    # Create a DynamicFrame using the 'persons_json' table
-    df = glueContext.create_data_frame.from_catalog(database=database, table_name=input_table)
+    # Create a dataframe from glue catalog
+    #df = glueContext.create_data_frame.from_catalog(database=database, table_name=input_table)
 
-    # Print out information about this data
+    #Print out information about this data
     #print("Count:  ", df.count())
     #df.printSchema()
+
+    df = spark.read.csv(input_s3_uri, header=True)
 
     # replace each tweeters name with crc bigint
     dfAnnocrc = df.withColumn("annonym", sha2("name", 256)).select("annonym", "tweet_id", "airline", "airline_sentiment", "text")
@@ -35,4 +41,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
 
